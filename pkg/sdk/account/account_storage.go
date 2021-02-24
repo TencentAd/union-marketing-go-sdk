eee947498e108ccfdef9b3eab492f4ac8de85b2a
linceyou <linceyou@tencent.com>
2021-02-24 20:01:48 +0800

    fix codecc problem

--- pkg/sdk/account/account_storage.go
+++ pkg/sdk/account/account_storage.go
@@ -4,6 +4,7 @@ import (
 	"git.code.oa.com/tme-server-component/kg_growth_open/api/sdk"
 )
 
+// Storage 账号存储接口
 type Storage interface {
 	// Upsert 插入或者更新一条授权的账号信息
 	Upsert(authAccount *sdk.AuthAccount) error
@@ -14,4 +15,3 @@ type Storage interface {
 	// List 列出已经授权的账号信息
 	List() ([]*sdk.AuthAccount, error)
 }
-

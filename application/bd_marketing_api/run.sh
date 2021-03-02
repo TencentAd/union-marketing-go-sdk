70f0e52357923e23f31f65582aba0a0441023f37
lyn <lynlinpeng@tencent.com>
2021-03-02 10:17:04 +0800

    删除代码库里面的application目录

--- application/bd_marketing_api/run.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/local/services/marketing_api_tool-1.0/bin/
-./marketing_api_tool -datetime=0 -sleep=false -condition="STAT_GROUP_BY_TIME_HOUR,STAT_GROUP_BY_AD_ID" >> ../log/marketing_api_tool.log
-./marketing_api_tool -datetime=0 -sleep=false -condition="STAT_GROUP_BY_TIME_HOUR,STAT_GROUP_BY_CREATIVE_ID,STAT_GROUP_BY_INVENTORY" >> ../log/marketing_api_tool.log
